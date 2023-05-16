ca207746bc545a5283d484de835fabea4606b16b
EverFlow <seeyyy@qq.com>
2023-05-16 16:07:10 +0800

    更新 RunLog.cs

--- TextHanding/RunLog.cs
+++ TextHanding/RunLog.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace YueHuan.System
+﻿namespace YueHuan.System
 {
     internal class RunLog
     {
